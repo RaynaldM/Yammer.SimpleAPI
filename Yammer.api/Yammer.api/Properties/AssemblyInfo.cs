5abc5f70affa71bf3a6cc746a7893591816e29c9
RaynaldM_cp <SND\RaynaldM_cp>
2013-10-28 22:24:26 +0000

    correct version

--- Yammer.api/Yammer.api/Properties/AssemblyInfo.cs
+++ Yammer.api/Yammer.api/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Yammer.SimpleAPI")]
-[assembly: AssemblyCopyright("Copyright ©  Raynald Messié 2013=2015")]
+[assembly: AssemblyCopyright("Copyright ©  Raynald Messié 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
